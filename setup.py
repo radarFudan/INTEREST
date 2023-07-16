ca0ed55d6fc0986ae0b070d948c826f35f5d0ab6
Altair <25528016+radarFudan@users.noreply.github.com>
2023-07-16 11:45:39 +0800

    copied from curse of memory

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Temporally weighted error in sequence modelling",
+    author="Wang Shida",
+    author_email="e0622338@u.nus.edu",
+    url="https://github.com/radarFudan/Curse-of-memory",
+    install_requires=["lightning", "hydra-core"],
+    packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    entry_points={
+        "console_scripts": [
+            "train_command = src.train:main",
+            "eval_command = src.eval:main",
+            "perturb_command = src.perturb:main",
+            "memory_command = src.memory:main",
+        ]
+    },
+)
